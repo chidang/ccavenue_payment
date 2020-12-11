2f669c3702b7a6d3f86bba5f4a9e378dcce7a9a9
Dang Chi <chid@jobready.com.au>
2020-12-11 22:09:15 +0700

    Update

--- lib/ccavenue_payment.rb
+++ lib/ccavenue_payment.rb
@@ -3,22 +3,20 @@ require 'ccavenue_payment/crypto'
 require 'ccavenue_payment/config'
 
 class CcavenuePayment
-   class << self
-    # Create request object
-    # === Options(Hash)
-    def encryped_data(data)
-      data.merge!({ merchant_id: Config.merchant_id }).to_query
-      crypto.encrypt(data)
-    end
+  # Create request object
+  # === Options(Hash)
+  def self.encryped_data(data)
+    data.merge!({ merchant_id: Config.merchant_id }).to_query
+    crypto.encrypt(data)
+  end
 
-    def decrypted_data(encrypted_string)
-      crypto.decrypt(encrypted_string)
-    end
+  def self.decrypted_data(encrypted_string)
+    crypto.decrypt(encrypted_string)
+  end
 
-    private
+  private
 
-    def crypto
-      Crypto.new(working_key: Config.working_key)
-    end
+  def self.crypto
+    Crypto.new(working_key: Config.working_key)
   end
 end
